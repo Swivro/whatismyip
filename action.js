d27f6e2c36508c46a7d95095ba3d05a9ed8a058f
Kasper Ireland <54741819+kasperireland@users.noreply.github.com>
2021-02-11 10:42:59 -0600

    Create action.js

---
+++ action.js
@@ -0,0 +1,16 @@
+let ip          = "";
+let sounds      = new Array();
+let curSound    = null;
+let START_POINT = -1;
+let END_POINT   = -1;
+const main = (json)=> {
+    ip = json.ip;
+    END_POINT = ip.length;
+
+   
+}
+
+
+
+
+
